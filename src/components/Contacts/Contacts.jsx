e2e3fb7c6da35c02f242deb334231bb3bdc697b8
Jurek Zelabowski <122160360+JurekZela@users.noreply.github.com>
2025-07-12 16:23:06 +0200

    new styles for btn remove button

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,18 +1,20 @@
 import { ListContact, RemoveButton } from './Contacts-styled';
 
 export const ContactList = ({ list, removeContact }) => {
-    return (
-        <ul>
-        {
-        list.map(({ id, name, number }) => {
-          return (
+  return (
+    <ul>
+      {list.map(({ id, name, number }) => {
+        return (
           <div key={id}>
-          <ListContact>{name}: {number}</ListContact>
-          <RemoveButton onClick={() => removeContact(id)}>Delete</RemoveButton>
+            <ListContact>
+              {name}: {number}
+            </ListContact>
+            <RemoveButton onClick={() => removeContact(id)}>
+              Delete
+            </RemoveButton>
           </div>
-          )
-        })
-        }
-        </ul>
-    )
-}
+        );
+      })}
+    </ul>
+  );
+};
