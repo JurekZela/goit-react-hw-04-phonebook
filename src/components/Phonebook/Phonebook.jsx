cb44257702ba80a622714881538f3d2988939fde
Jurek Zelabowski <122160360+JurekZela@users.noreply.github.com>
2025-07-31 15:17:54 +0200

    fixed

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,49 +1,54 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
-import { Button, StyledForm as Form, StyledField as Field, StyledLabel as Label, ErrorMsg, } from './PhoneBook-styled';
+import {
+  Button,
+  StyledForm as Form,
+  StyledField as Field,
+  StyledLabel as Label,
+  ErrorMsg,
+} from './PhoneBook-styled';
 
 const SignupSchema = Yup.object().shape({
-    name: Yup.string()
-      .min(4, 'Too Short!')
-      .max(40, 'Too Long!')
-      .required('Required'),
-    number: Yup.number()
-      .min(9, 'Too Short!')
-      .required('Required'),
-  });
-  
+  name: Yup.string()
+    .min(4, 'Too Short!')
+    .max(40, 'Too Long!')
+    .required('Required'),
+  number: Yup.number().min(9, 'Too Short!').required('Required'),
+});
 
-export default function ContactForm ({ onAdd }) {
+export default function ContactForm({ onAdd }) {
   return (
-    <Formik
-    initialValues={{
-      name: '',
-      number: '',
-      contacts: [],
-    }}
-    validationSchema={SignupSchema}
-    onSubmit={async function (values, actions) {
-      await new Promise((r) => setTimeout(r, 500));
-      onAdd({ id: nanoid(), ...values });
-      actions.resetForm();
-    }}
-  >
-    <Form>
-      <Label>
-        Name
-        <Field  name="name" placeholder="Phil Collins" />
-        <ErrorMsg name="name" component="span" />
-        </Label>
+    <div>
+      <Formik
+        initialValues={{
+          name: '',
+          number: '',
+          contacts: [],
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={async function (values, actions) {
+          await new Promise(r => setTimeout(r, 500));
+          onAdd({ id: nanoid(), ...values });
+          actions.resetForm();
+        }}
+      >
+        <Form>
+          <Label>
+            Name
+            <Field name="name" placeholder="Phil Collins" />
+            <ErrorMsg name="name" component="span" />
+          </Label>
 
-      <Label>
-        Number
-        <Field type="tel"  name="number" placeholder="+48-000-000-000" />
-        <ErrorMsg name="number" component="span" />
-        </Label>       
+          <Label>
+            Number
+            <Field type="tel" name="number" placeholder="+48-000-000-000" />
+            <ErrorMsg name="number" component="span" />
+          </Label>
 
-      <Button type="submit">Add contact</Button>
-    </Form>    
-  </Formik>
-  )
-};
+          <Button type="submit">Add contact</Button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
